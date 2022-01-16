3ec9a8d5c10ad9c4735ac6e4f21ec2a456e7e673
RDWardWizard <68459573+RDWardWizard@users.noreply.github.com>
2022-01-16 13:38:13 +0530

    opto-couplers + push button

--- 39_stepper_motor.ino
+++ 39_stepper_motor.ino
@@ -4,7 +4,13 @@ const int stepPin = 15  ;
 const int dirPin = 2; 
 const int enPin = 18;
 bool dir = true;
-int numberOfRevolutions = 95;
+//bool lastDir = true;
+int numberOfRevolutions = 95; // reducing from 95 to 5 for development purpose.
+
+const byte optoOne = 13; // The first opto-coupler has been connected with the GPIO-13.
+const byte optoTwo = 12; // The second opto-coupler has been connected with the GPIO-12.
+const byte inOrOut = 14; // Button to get the battery out if it is in and to get it in if it is out.
+bool rotate = false;
 
 void setup() {
   Serial.begin(9600);
@@ -12,26 +18,55 @@ void setup() {
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
+  pinMode(inOrOut,INPUT_PULLUP);
   digitalWrite(dirPin,HIGH);
   digitalWrite(enPin,LOW);
 
-  for(int x = 0; x < 3200; x++) {
-      digitalWrite(stepPin,HIGH); 
-      delayMicroseconds(50); 
-      digitalWrite(stepPin,LOW); 
-      delayMicroseconds(50);
-  }
+  attachInterrupt(digitalPinToInterrupt(optoOne),stopAndReverse,RISING);
+  attachInterrupt(digitalPinToInterrupt(optoTwo),stopAndForward,RISING);
+  attachInterrupt(digitalPinToInterrupt(inOrOut),start,FALLING);  
 }
-void loop() {
-  for(int i = 0; i < numberOfRevolutions; i++){
-    for(int x = 0; x < 3200; x++) {
-      digitalWrite(stepPin,HIGH); 
-      delayMicroseconds(25); 
-      digitalWrite(stepPin,LOW); 
-      delayMicroseconds(25);
-    }
-  }
-  delay(1000);
-  dir = !dir;
-  digitalWrite(dirPin, dir);
+
+void stopAndReverse(){
+  dir = false;
+  digitalWrite(dirPin,dir);
+  rotate = false;
+  //numberOfRevolutions = 0;
+}
+
+void stopAndForward(){
+  dir = true;
+  digitalWrite(dirPin,dir);
+  rotate = false;
+  //numberOfRevolutions = 0;
+}
+
+void start(){
+  //dir = !dir;
+  //digitalWrite(dirPin,dir);
+  rotate = true;
+}
+
+void loop(){
+  //if(numberOfRevolutions == 0){
+    for(int i = 0; i < numberOfRevolutions; i++){
+      for(int x = 0; x < 3200; x++) {
+        if(rotate == true){
+          digitalWrite(stepPin,HIGH); 
+          delayMicroseconds(50); 
+          digitalWrite(stepPin,LOW); 
+          delayMicroseconds(50);
+        }
+        else{
+          break;
+        }
+      }
+      if(rotate == true && i == (numberOfRevolutions - 1)){
+        dir = !dir;
+        digitalWrite(dirPin,dir);
+        rotate = false;
+      }
+      if(rotate == false) break;
+     }
+  //}
 }
